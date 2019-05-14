958586e02fa4741f2d2d98bcb6a11e7d7ad83d9d
Narayan Rai <narayanrai97@gmail.com>
2019-05-14 11:50:23 -0400

    Modify offense seedings

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,45 +3,44 @@ require 'csv'
 
 # read CSV file into a variable
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'offenses.csv'))
+
 # Converts the CSV into a structure that Ruby can read.
 csv = CSV.parse(csv_text, headers: true, encoding: 'UTF-8')
+
 # Loop through the entire CSV file and convert each row of thr document
 # into a hash. The headers of the CVS file are used as keys for the hash
-
 csv.each do |row|
-  t = Offense.new
-  t.first_name = row['first_name']
-  t.middle_name = row['middle_name']
-  t.last_name = row['last_name']
-  t.gender = row['gender']
-  t.race = row['race']
-  t.address_number = row['address_number']
-  t.street = row['street']
-  t.zip = row['zip']
-  t.county = row['county']
-  t.state = row['state']
-  t.file_number = row['file_number']
-  t.offense_date = row['offense_date']
-  t.disposition = row['disposition']
-  t.disposition_date = row['disposition_date']
-  t.county_disposition = row['county_disposition']
-  t.arrest_agency_name = row['arrest_agency_name']
-  t.arrest_agency_department = row['arrest_agency_department']
-  t.arrest_agency_address_number = row['arrest_agency_address_number']
-  t.arrest_agency_street = row['arrest_agency_street']
-  t.dob = row['dob']
-  t.offense_description = row['offense_description']
-  t.arrest_date = row['arrest_date']
-  t.arrest_agency_zip = row['arrest_agency_zip']
-  # t.save
+  offense = Offense.new
+  offense.file_number = row['File Number']
+  offense.first_name = row['First Name']
+  offense.middle_name = row['Middle Name']
+  offense.last_name = row['Last Name']
+  offense.gender = row['Gender']
+  offense.race = row['Race']
+  offense.dob = row['Date of Birth']
+  offense.address_number = row['Street Number']
+  offense.street = row['Street Name']
+  offense.zip = row['Zip']
+  offense.county = row['County']
+  offense.state = row['State']
+  offense.offense_description = row['Offense Description']
+  offense.offense_date = row['Offense Date']
+  offense.disposition = row['Disposition']
+  offense.disposition_date = row['Disposition Date']
+  offense.county_disposition = row['County Disposition']
+  offense.arrest_date = row['Arrest Date']
+  offense.arrest_agency_name = row['Arrest Agency Name']
+  offense.arrest_agency_department = row['Arrest Agency Department']
+  offense.arrest_agency_address_number = row['Arrest Agency Street Number']
+  offense.arrest_agency_street = row['Arrest Agency Street Name']
+  offense.arrest_agency_zip = row['Arrest Agency Zip']
 
-  if t.save
-
+  if offense.save
+    puts "Offense #{offense.file_number} saved!"
   else
-    t.errors
+    offense.errors
   end
 
-  puts "Offense #{t.first_name} saved!"
 end
 
 puts "There are now #{Offense.count} rows in the Offense table"
