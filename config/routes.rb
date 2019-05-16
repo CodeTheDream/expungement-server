d34a27f36e6ff788a28d82ddca0d8b4751aba503
kwasi yeboah <yjkwasi007@gmail.com>
2019-05-16 17:10:04 +0000

    wrote testing for Devise log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :offenses
   devise_for :admins
+  root 'offenses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,19 +1,29 @@
 #feature sign in
 
-RSpec.describe 'Sign in', :devise, js: true do
-  include Features::SessionHelpers
-  
-scenario "Only admin users already having an account can sign in" do
-it "signs in" do
-  signin('a1@sample.com', 'password')
-    expect(page).to have_content I1n.t 'devise.failure.invalid', authentication_keys: 'email'
-  end
-end
+require 'rails_helper'
 
-scenario 'user can sign in' do
-  
-    @admin = create(:admin)
-    signin(@admin.email, @admin.password)
-    expect(page).to have_content I1n.t 'devise.sessions.signed_in'
+RSpec.feature "Sign in", type: :feature do
+include Warden::Test::Helpers
+# this feature allows you to create a login 
+    def setup
+     @admin = FactoryBot.create(:admin)
+    end
+
+
+it "should sign up as a user", js: true do
+  setup
+    visit '/admins/sign_in'
+    save_screenshot('flag1.png')
+    fill_in "Email", with: "a1@sample.com"
+    fill_in "Password", with: "password"
+    save_screenshot("flag2.png")
+    click_button "Log in" 
+    sleep 2
+    save_screenshot("flag3.png")
+    expect(page).to have_content("Offenses")
+    click_link "log out"
+    save_screenshot("flag4.png")
+    expect(page).to have_content("Sign in")
+
+    end
   end
-end
