c94e8fb094080266daf408a8a074c49755a48366
Narayan Rai <narayanrai97@gmail.com>
2019-05-29 14:00:02 -0400

    Add more examples to the tests

--- spec/requests/offenses_api_spec.rb
+++ spec/requests/offenses_api_spec.rb
@@ -17,39 +17,44 @@ RSpec.describe 'Offenses API', type: :request do
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
-
   end
 
   describe 'GET /offenses when `first_name` is passed' do
-    # byebug
-    before { get '/api/v1/offenses',
-              params: { "q"=> '{"first_name":"John"}' }
-            }
+    before { get '/api/v1/offenses', params: { "q"=> '{"first_name":"John"}' }}
 
     it 'returns offenses having `John`' do
       expect(json).not_to be_empty
       expect(json.size).to eq(2)
     end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 
   describe 'GET /offenses when `first_name` and `last_name` are passed' do
-    before { get '/api/v1/offenses',
-              params: { "q"=> '{"first_name":"John", "last_name":"Doe"}'}
-            }
+    before { get '/api/v1/offenses', params: { "q"=> '{"first_name":"John", "last_name":"Doe"}'}}
 
     it 'returns offenses having `John` and `Doe`' do
       expect(json).not_to be_empty
       expect(json.size).to eq(2)
     end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 
   describe 'GET /offenses when `first_name`, `last_name`, and `middle_name` are passed' do
-    before { get '/api/v1/offenses',
-              params: { "q"=> '{"first_name":"John","last_name":"Doe","middle_name":"Bahadur"}'}
-            }
+    before { get '/api/v1/offenses', params: { "q"=> '{"first_name":"John","last_name":"Doe","middle_name":"Bahadur"}'}}
+
     it 'returns offenses having `John`, `Doe`, `Bahadur`' do
       expect(json).not_to be_empty
       expect(json.size).to eq(1)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
     end
   end
 end
