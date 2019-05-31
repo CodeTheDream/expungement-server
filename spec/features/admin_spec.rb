a9e68df83ef0a2e8498f6e93c97d878f27ef5c2f
Narayan Rai <narayanrai97@gmail.com>
2019-05-31 14:25:25 -0400

    Fix admin_spec.rb

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,10 +1,8 @@
-#feature sign in
-
 require 'rails_helper'
 
 RSpec.feature "Sign in", type: :feature do
 include Warden::Test::Helpers
-# this feature allows you to create a login 
+# this feature allows you to create a login
     def setup
      @admin = FactoryBot.create(:admin)
     end
@@ -12,16 +10,16 @@ include Warden::Test::Helpers
 
 it "should sign up as a user", js: true do
   setup
-    visit '/admins/sign_in'
+    visit '/admin/sign_in'
     save_screenshot('flag1.png')
     fill_in "Email", with: "a1@sample.com"
     fill_in "Password", with: "password"
     save_screenshot("flag2.png")
-    click_button "Log in" 
+    click_button "Log in"
     sleep 2
     save_screenshot("flag3.png")
-    expect(page).to have_content("Offenses")
-    click_link "log out"
+    expect(page).to have_content("Expungement")
+    click_link "Log out"
     save_screenshot("flag4.png")
     expect(page).to have_content("Sign in")
 
